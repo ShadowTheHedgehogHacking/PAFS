a4b6398115e7ceecaa68cbf8297fc2fb377f1cfb
dreamsyntax <dreamsyntax@gmail.com>
2022-09-10 16:13:00 -0700

    .net cli template

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using AFSLib;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("PAFS: Patch AFS");
+        Console.WriteLine("Usage: PAFS <filenames of AFS>");
+        Console.WriteLine("Example: \"PAFS PJS.AFS\" would use PJS.AFS and check \"PJS\" directory for file replacement names, resulting in PATCHED_PJS.AFS output file");
+
+        if (args.Count() < 2 || args.Count() > 2)
+        {
+            Console.WriteLine("Invalid arguments");
+        }
+        foreach (var arg in args)
+        {
+            //var file = Path.GetFullPath(arg);
+        }
+    }
+}
